d028b8546961dc9ef61e4fe91f6b61579fd1e87a
Jack Bergemann <jack@zierniemann.com>
2020-12-05 12:55:04 -0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main')
+
+  @section('content')
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5">Task Manager</h1>
+          <p class="lead"></p>
+              <form>
+                  <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+        </div>
+      </div>
+    </div>
+  @endsection
